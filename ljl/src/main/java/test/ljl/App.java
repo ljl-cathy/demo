1b9c4b842e515b951c3a4aff967f6bbb08e5729b
ljl-cathy <718121502@qq.com>
2017-01-17 09:33:07 +0800

    Add files via upload

---
+++ ljl/src/main/java/test/ljl/App.java
@@ -0,0 +1,13 @@
+package test.ljl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
